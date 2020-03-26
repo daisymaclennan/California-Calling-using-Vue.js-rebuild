68462b4c97e3ee203f3ceeba92f0c20fdb2358f9
Daisy Maclennan <daisy.maclennan@distinction.co.uk>
2020-03-26 15:55:56 +0000

    Added banner, commented out non working Vanilla JS animation code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer>
-    <!--<section>
-      <h1 class="date-list">June  21, <span>2019</span> June  21, <span>2019</span> June  21, <span>2019</span></h1>
-      <h1 class="date-list">June  21, <span>2019</span> June  21, <span>2019</span> June  21, <span>2019</span></h1>
-    </section>-->
+    <section class="rotating-banner">
+      <h1 class="date-list--one">June  21, <span>2019</span> June  21, <span>2019</span> June  21, <span>2019</span></h1>
+      <h1 class="date-list--two">June  21, <span>2019</span> June  21, <span>2019</span> June  21, <span>2019</span></h1>
+    </section>
     <section class="footer-content">
       <p>Join us</p>
       <h2 style="">Mark your calendar for our SF Design Week Studio Crawl. In addition to custom swag, we’ll also have a DJ, a mixologist, and other cool surprises we can’t tell you about yet.</h2>
@@ -16,6 +16,37 @@
 export default {
   name: 'Footer'
 }
+
+/*const dateOne = document.querySelector('date-list--one')
+const dateTwo = document.querySelector('date-list--two')
+
+function infinityRotateBackwards(titleOne, titleTwo, duration){
+  //Gets the width of each title
+  const textWidth = titleOne.getBoundingClientRect().width;
+
+  //Starts at original position and ends at negative its width
+  titleOne.animate([
+  { transform: 'translateX(0px)' },
+  { transform: 'translateX(-' + textWidth + 'px)' }
+], {
+  // timing options
+  duration: duration,
+  iterations: Infinity,
+  easing: 'cubic-bezier(0, 0, 0, 0)',//constant speed
+})
+  //Starts at where the end of the first title would end then returns to its original position
+  titleTwo.animate([
+    { transform: 'translateX(' + textWidth + 'px)' },
+    { transform: 'translateX(0px)' }
+  ], {
+    // timing options
+    duration: duration,
+    iterations: Infinity,
+    easing: 'cubic-bezier(0, 0, 0, 0)',//constant speed
+  })
+}
+
+infinityRotateBackwards(dateOne, dateTwo, 1)*/
 </script>
 
 <style scoped>
@@ -29,6 +60,26 @@ footer{
 .footer-content{
   padding-top: 240px;
   padding-bottom: 80px;
+}
+
+.rotating-banner{
+  background-color: #eee;
+  color: #000;
+  transform: rotate(45deg);
+  padding: 8px 0;
+  width: 150%;
+  margin-top: 10vh;
+  position: absolute;
+  text-align: center;
+  white-space: nowrap;
+  display: flex;
+  flex-direction: row;
+  text-transform: uppercase;
+}
+
+.rotating-banner span{
+  -webkit-text-stroke: 1px #000;
+  color: hsla(0,0%,100%,0);
 }
 
 @media screen and (min-width: 1000px){
